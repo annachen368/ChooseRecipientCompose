641d6fd911e1659699d43f74878d3ab41f6b77ec
anna <annachen368@gmail.com>
2025-03-25 16:22:18 -0700

    feat: Update WelcomeScreen layout to include loading indicator and error handling

--- app/src/main/java/com/example/chooserecipientcompose/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/chooserecipientcompose/ui/welcome/WelcomeScreen.kt
@@ -1,10 +1,15 @@
 package com.example.chooserecipientcompose.ui.welcome
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -13,23 +18,29 @@ fun WelcomeScreen(
     viewModel: WelcomeViewModel = hiltViewModel(),
     navigateToChooseRecipient: () -> Unit
 ) {
-    when (val state = viewModel.uiState.value) {
-        is WelcomeViewModel.UiState.Loading -> {
-            // Show loading indicator
-            CircularProgressIndicator()
-        }
-
-        is WelcomeViewModel.UiState.Success -> {
-            Text(text = state.message)
-            Button(onClick = {
-                navigateToChooseRecipient()
-            }) {
-                Text(text = "Go to Choose Recipient")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding), contentAlignment = Alignment.Center
+    ) {
+        when (val state = viewModel.uiState.value) {
+            is WelcomeViewModel.UiState.Loading -> {
+                // Show loading indicator
+                CircularProgressIndicator()
             }
-        }
 
-        is WelcomeViewModel.UiState.Error -> {
-            Text(text = state.message, color = androidx.compose.ui.graphics.Color.Red)
+            is WelcomeViewModel.UiState.Success -> {
+                Text(text = state.message)
+                Button(onClick = {
+                    navigateToChooseRecipient()
+                }) {
+                    Text(text = "Go to Choose Recipient")
+                }
+            }
+
+            is WelcomeViewModel.UiState.Error -> {
+                Text(text = state.message, color = androidx.compose.ui.graphics.Color.Red)
+            }
         }
     }
 }
